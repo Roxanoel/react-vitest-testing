cfc99c653b421c501986a15bab918e766a2b86d0
Roxane Noël <roxane.noel92@gmail.com>
2024-02-08 11:13:48 -0500

    Setting up vitest

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -3,22 +3,9 @@ import { render, screen } from '@testing-library/react';
 
 import App from '../App';
 
-describe('App', () => {
-  it('renders headline', () => {
-    render(<App title="React" />);
-
-    screen.debug();
-
-    // check if App components renders headline
-  });
-});
-
-describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
-  });
-
-  it('false to be false', () => {
-    expect(false).toBe(false);
+describe("App component", () => {
+  it("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
   });
 });
