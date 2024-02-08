b027d788c09963c9f990d00a19e97b63c823749c
Roxane Noël <roxane.noel92@gmail.com>
2024-02-08 13:32:28 -0500

    userEvent testing

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,11 +1,20 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 
 import App from '../App';
 
 describe("App component", () => {
   it("renders correct heading", () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders radical rhinos after button press", async () => {
+    const user = userEvent.setup();
     render(<App />);
-    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+    const button = screen.getByRole("button");
+    await user.click(button);
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
   });
 });
